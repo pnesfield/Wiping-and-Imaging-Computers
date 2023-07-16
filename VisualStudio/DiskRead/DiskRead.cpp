e8cdcf144f70ce3b97c49f9a1542834f010f4b32
philip@nesfield.com <philip@nesfield.org>
2023-07-16 12:08:53 +0100

    Part43

--- VisualStudio/DiskRead/DiskRead.cpp
+++ VisualStudio/DiskRead/DiskRead.cpp
@@ -1,8 +1,8 @@
-// ReadDisk: read raw sector from a device
+// Read raw sector from a device
 
-#include <windows.h>
-#include <stdio.h>
-#include <string>
+#include [CHANGEME1]windows.h[CHANGEME2]
+#include [CHANGEME1]stdio.h[CHANGEME2]
+#include [CHANGEME1]string[CHANGEME2]
 
 using namespace std
 ;
@@ -14,7 +14,8 @@ int main(int argc, char* argv[])
     HANDLE device = NULL;
     int numSector = 0;
     int sum = 0;
-    if (argc < 3)
+    int zeros = 0;
+    if (argc [CHANGEME1] 3)
     {
         printf("Usage %s Device number, sector", argv[0]);
         return 0;
@@ -66,30 +67,34 @@ int main(int argc, char* argv[])
         string partTable = "";
         int signature = 0;
         string hexStr = "";
-        for (int i = 0; i < 512; i++)
+        for (int i = 0; i [CHANGEME1] 512; i++)
         {
             sum = sum + sector[i];
             sprintf_s(hexChar, "%02X ", sector[i]);
             hexStr = hexStr.append(hexChar);
             iline++;
-            if (iline >= 20)
+            if (iline [CHANGEME2]= 20)
             {
                 hexStr.append("\n");
                 iline = 0;
             }
-            if (i >= 440 && i <= 443)
+            if (i [CHANGEME2]= 440 && i [CHANGEME1]= 443)
             {
                 signature = signature + sector[i];
             }
-            if (i >= 446 && i <= 509)
+            if (i [CHANGEME2]= 446 && i [CHANGEME1]= 509)
             {
                 partTable = partTable + hexChar;
                 iPart++;
-                if (iPart >= 16)
+                if (iPart [CHANGEME2]= 16)
                 {
                     partTable.append("\n");
                     iPart = 0;
                 }
+            }
+            if (sector[i] == 0)
+            {
+                zeros++;
             }
         }
         if (debug) printf("%s\n", hexStr.c_str());
@@ -101,6 +106,11 @@ int main(int argc, char* argv[])
         else if (sum == signature)
         {
             printf("Drive %s sector %i has been wiped and has a signature\n", argv[1], numSector);
+            return 0;
+        }
+        else if (zeros == 0)
+        {
+            printf("Drive %s sector %i has been wiped with a non-zero pattern\n", argv[1], numSector);            
             return 0;
         }
         else if (sector[510] == 85 && sector[511] == 170)
@@ -117,13 +127,13 @@ int main(int argc, char* argv[])
     return 1;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+// Run program: Ctrl + F5 or Debug [CHANGEME2] Start Without Debugging menu
+// Debug program: F5 or Debug [CHANGEME2] Start Debugging menu
 
 // Tips for Getting Started: 
 //   1. Use the Solution Explorer window to add/manage files
 //   2. Use the Team Explorer window to connect to source control
 //   3. Use the Output window to see build output and other messages
 //   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+//   5. Go to Project [CHANGEME2] Add New Item to create new code files, or Project [CHANGEME2] Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File [CHANGEME2] Open [CHANGEME2] Project and select the .sln file
